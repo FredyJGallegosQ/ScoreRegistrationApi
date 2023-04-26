4a65c26c0c9494a1ea304978f63175254dcca758
Fredy <120886@unsaac.edu.pe>
2023-04-26 17:33:04 -0500

    More Controllers

--- Controllers/AlternativesController.cs
+++ Controllers/AlternativesController.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RegistroNotasApi.Models;
 
 namespace RegistroNotasApi.Controllers
 {
+    [ApiController]
+    [Route("[controller]")]
     public class AlternativesController
     {
-        
+        private Context _contex;
+        public AlternativesController(Context context){
+            _contex = context;
+        }
+        [HttpGet]
+        public IEnumerable<Alternatives> get(){
+            return _contex.alternatives.ToList();
+        }
+        [HttpPost]
+        public Alternatives post(Alternatives alternatives){
+            _contex.alternatives.Add(alternatives);
+            _contex.SaveChanges();
+            return alternatives;
+        }
     }
 }
--- Controllers/AnswerController.cs
+++ Controllers/AnswerController.cs
@@ -2,11 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RegistroNotasApi.Models;
 
 namespace RegistroNotasApi.Controllers
 {
-    public class AnswerController
+    [ApiController]
+    [Route("[controller]")]
+    public class AnswerController: ControllerBase
     {
-        
+        private Context _context;
+        public AnswerController(Context context){
+            _context = context;
+        }
+        [HttpGet]
+        public IEnumerable<Answer> get(){
+            return _context.answers.ToList();
+        }
+        [HttpPost]
+        public Answer post(Answer answer){
+            _context.answers.Add(answer);
+            _context.SaveChanges();
+            return answer;
+        }
     }
 }
